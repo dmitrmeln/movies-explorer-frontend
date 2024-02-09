99f81179e16d8a6e871585cef9503d892ef2dabe
Dmitrii Melnichuk <melnichuk176@gmail.com>
2024-02-09 05:55:56 +0300

    name fixed

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 const {
-  REACT_APP_API_URL='https://api.movies.nomoredomainsmonster.ru/',
+  REACT_APP_API_URL='https://api.movies-dip.nomoredomainsmonster.ru/',
   BEAT_FILM_MOVIES_API_URL='https://api.nomoreparties.co/',
 } = process.env;
 
